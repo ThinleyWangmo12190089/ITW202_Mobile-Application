a53f9eb2d42efe58c306fd62dcf4122aa9cb9fe6
Thinley Wangmo <12190089.gcit@rub.edu.bt>
2021-05-10 22:55:33 +0530

    todo15 one

---
+++ TODO_15i/app/src/main/java/com/example/todo_15i/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.todo_15i;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Top Stories"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tech News"));
+        tabLayout.addTab(tabLayout.newTab().setText("Cooking"));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) { }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) { }
+        });
+
+    }
+}
